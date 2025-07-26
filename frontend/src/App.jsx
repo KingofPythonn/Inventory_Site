f61add59b5b5170dee85330759248547478da7ee
Sina Abbaszadeh <sinaabbaszadeh07@gmail.com>
2025-07-26 13:18:04 +0200

    start frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+   <div className='text-3xl font-bold underline text-red-500'>
+    React APP
+   </div>
+
+
+  )
+}
+
+export default App
